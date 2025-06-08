8b40f9f249fdebe7316cd4f2994b73448336512c
ayushmanAuditors <ayushman@auditorsdesk.in>
2025-06-08 15:38:03 +0530

    passing elements as props in Component

---
+++ src/components/vitest-1/grandparent.vue
@@ -0,0 +1,17 @@
+<script>
+import Child from './child.vue';
+export default {
+  name: 'Grandparent',
+  data() {
+    return {
+    }
+  },
+  components: {
+    Child
+  }
+}
+</script>
+
+<template>
+  <Child :msg="'Hola'" :element="'p'"/>
+</template>
---
+++ src/components/vitest-1/parent.vue
@@ -0,0 +1,17 @@
+<script>
+import Child from './child.vue';
+export default {
+  name: 'Parent',
+  data() {
+    return {
+    }
+  },
+  components: {
+    Child
+  }
+}
+</script>
+
+<template>
+  <Child :msg="'Hello'" :element="'h1'"/>
+</template>
